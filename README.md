32d97be9af162198c3a1a5314d142165c92aa283
Sapphire Becker <sapphire.becker@janrain.com>
2018-12-19 12:57:17 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# aws-dart - A companion for aws-vault #
+When you're a global company with a few IAM roles in a bunch of regions and specifying the region manually every time is tedious.
+
+## Requirements ##
+* [aws-vault](https://github.com/99designs/aws-vault)
+* `pip install -r requirements.txt`
+
+## Usage ##
+In your `~/.aws/config` file, add some **target**s. These allow you to specify the config settings you wish to override while inheriting the rest from the profile it references.
+
+Each **target** entry must contain a **profile** key that contains the name of a defined profile.
+
+```ini
+# This profile is what references your credentials in aws-vault
+[profile id]
+mfa_serial = arn:aws:iam::123456789012:mfa/user
+
+# This profile is what you normally pass to aws-vault
+[profile dev]
+source_profile = id
+role_arn = arn:aws:iam:...
+mfa_serial = arn:aws:iam::123456789012:mfa/user
+
+# This is what you will pass to aws-dart
+[target dev-us]
+profile = dev
+region = us-east-1
+```
+
+Run the command such as `aws-dart exec dev-us env | grep AWS_REGION`
+
+It will run `aws-vault exec dev env | grep AWS_REGION` but your grep will result in `AWS_REGION=us-east-1` instead of nothing.
+
+## How it works ##
+This generates a new config file containing your **defaults** header and a profile that combines the target with its **profile**. Then it sets the `AWS_CONFIG_FILE` environment variable before running the aws-vault command.
+
+The new config is written to the same location as the last one but with the target name appended. So if you're using the default location, `~/.aws/config`, it would write to `~/.aws/config_dev-us`
+
+Thus all target names should be filename safe. (It's not like you wanted to quote the argument on the CLI anyway, right?)
+
+## Installation ##
+Download this and link the `aws-vault.py` file somewhere in a folder in your $PATH
+
+On linux I have a user-level bin folder in my $PATH at `~/.local/bin` so I linked it in there with `ln -s ~/src/aws-dart/aws-dart.py ~/.local/bin/aws-dart`
---
+++ aws-dart.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+from botocore import configloader, exceptions
+
+
+def parse_aws_config(filename="~/.aws/config"):
+    real_filename = os.path.expanduser(filename)
+    return configloader.load_config(real_filename)
+
+
+def load_target(config, target_name):
+    key = "target {}".format(target_name)
+    if key in config:
+        target = config[key]
+        if "profile" not in target:
+            raise exceptions.MissingParametersError(object_name=target_name, missing="profile")
+
+        profile_name = target.pop("profile")
+        if profile_name not in config["profiles"]:
+            raise exceptions.ProfileNotFound(profile_name)
+
+        profile = config["profiles"][profile_name]
+        profile_key = "profile {}".format(profile_name)
+        temporary_config = profile.copy()
+        temporary_config.update(target)
+        temporary_config = { profile_key: temporary_config }
+
+        if "default" in config["profiles"]:
+            temporary_config["default"] = config["profiles"]["default"]
+
+        return profile_name, temporary_config
+
+
+def write_config(filename, temporary_config):
+    real_filename = os.path.expanduser(filename)
+    with open(real_filename, "w") as f:
+        for section_name, section in temporary_config.items():
+            print("[{}]".format(section_name), file=f)
+
+            for key, value in section.items():
+                if isinstance(value, dict):
+                    print("{} =".format(key), file=f)
+
+                    for dkey, dvalue in value.items():
+                        print("  {} = {}".format(dkey, dvalue), file=f)
+                else:
+                    print("{} = {}".format(key, value), file=f)
+
+
+def run_vault_with_config(profile, filename):
+    real_filename = os.path.expanduser(filename)
+    env = os.environ.copy()
+    env["AWS_CONFIG_FILE"] = real_filename
+    result = subprocess.run(("aws-vault", sys.argv[1], profile, *sys.argv[3:]), env=env)
+    return result.returncode
+
+
+if __name__ == "__main__":
+    config_file = os.getenv("AWS_CONFIG_FILE", "~/.aws/config")
+    config = parse_aws_config(config_file)
+
+    target_name = sys.argv[2]
+    profile, temporary_config = load_target(config, target_name)
+
+    filename = "{}_{}".format(config_file, target_name)
+    write_config(filename, temporary_config)
+
+    sys.exit(run_vault_with_config(profile, filename))
